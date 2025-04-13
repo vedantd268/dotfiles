692f788896cd01e8377ae2d478e904c54c7538fe
vedant <vedantd73839@gmail.com>
2025-04-13 16:26:19 +0530

    lualine config modified

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,26 +11,6 @@ return {
           lualine_a = { "mode" }, -- Typically mode goes in lualine_a
           lualine_b = { "branch", "diff", "diagnostics" },
           lualine_c = { "filename" }, -- Placed filename here
-          lualine_x = {
-            {
-              function()
-                return require("noice").api.statusline.mode.get()
-              end,
-              cond = function()
-                return require("noice").api.statusline.mode.has()
-              end,
-              color = { fg = "#ff9e64" },
-            },
-            {
-              function()
-                return require("noice").api.status.command.get()
-              end,
-              cond = function()
-                return require("noice").api.status.command.has()
-              end,
-              color = { fg = "#ff9e64" },
-            },
-          },
           lualine_y = { "encoding", "fileformat", "filetype" },
           lualine_z = { "location" },
         },
