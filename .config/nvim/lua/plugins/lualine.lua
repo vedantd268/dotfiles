4f2043245ca38e652048e4e08c050f178271daa3
vedant <vedantd73839@gmail.com>
2025-04-13 16:46:15 +0530

    lualine config nvim-webdev-icon added

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,20 +1,5 @@
 return {
   "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        component_separators = "|",
-        section_separators = "",
-
-        sections = {
-          lualine_a = { "mode" }, -- Typically mode goes in lualine_a
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" }, -- Placed filename here
-          lualine_y = { "encoding", "fileformat", "filetype" },
-          lualine_z = { "location" },
-        },
-      },
-    })
-  end,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {},
 }
