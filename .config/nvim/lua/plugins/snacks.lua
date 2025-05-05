d364df0697c9a00c466e730ff042bbf57fe09d62
vedant <vedantd73839@gmail.com>
2025-05-05 19:29:57 +0530

    snacks config modified

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -100,141 +100,110 @@ return {
       function()
         Snacks.picker.buffers()
       end,
-      desc = "Buffers",
     },
     {
       "<leader>g",
       function()
         Snacks.picker.grep({ layout = "default" })
       end,
-      desc = "Grep",
     },
     {
       "<leader>:",
       function()
         Snacks.picker.command_history({ layout = "select" })
       end,
-      desc = "Command History",
     },
     {
       "<leader>n",
       function()
         Snacks.picker.notifications()
       end,
-      desc = "Notification History",
     },
     {
       "<leader>e",
       function()
         Snacks.explorer()
       end,
-      desc = "File Explorer",
     },
-
-    -- find
     {
       "<leader>pc",
       function()
         Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
       end,
-      desc = "Find Config File",
     },
     {
       "<leader>pf",
       function()
         Snacks.picker.files()
       end,
-      desc = "Find Files",
     },
     {
       "<leader>pg",
       function()
         Snacks.picker.git_files()
       end,
-      desc = "Find Git Files",
     },
     {
       "<leader>pp",
       function()
         Snacks.picker.projects()
       end,
-      desc = "Projects",
     },
     {
       "<leader>pr",
       function()
         Snacks.picker.recent()
       end,
-      desc = "Recent",
     },
-
-    -- Grep
     {
       "<leader>gb",
       function()
         Snacks.picker.lines()
       end,
-      desc = "Buffer Lines",
     },
     {
       "<leader>gw",
       function()
         Snacks.picker.grep_word()
       end,
-      desc = "Visual selection or word",
       mode = { "n", "x" },
     },
-
-    -- search
     {
       "<leader>sd",
       function()
         Snacks.picker.diagnostics()
       end,
-      desc = "Diagnostics",
     },
     {
       "<leader>sh",
       function()
         Snacks.picker.help({ layout = "default" })
       end,
-      desc = "Help Pages",
     },
     {
       "<leader>sm",
       function()
-        Snacks.picker.marks()
+        Snacks.picker.marks({ layout = "default" })
       end,
-      desc = "Marks",
     },
     {
       "gd",
       function()
         Snacks.picker.lsp_definitions()
       end,
-      desc = "Goto Definition",
-    },
-    {
-      "gD",
-      function()
-        Snacks.picker.lsp_declarations()
-      end,
-      desc = "Goto Declaration",
     },
     {
       "gr",
       function()
-        Snacks.picker.lsp_references()
+        Snacks.picker.lsp_references({ layout = "default" })
       end,
       nowait = true,
-      desc = "References",
     },
     {
       "gI",
       function()
         Snacks.picker.lsp_implementations()
       end,
-      desc = "Goto Implementation",
     },
   },
 }
