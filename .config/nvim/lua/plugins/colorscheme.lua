b8a8e3f2a70e19a846f39b1f77930edc4b288740
vedant <vedantd73839@gmail.com>
2025-06-09 12:07:56 +0530

    many hl-group added and modified colors

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,38 +1,44 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    config = function()
-      require("kanagawa").setup({
-        compile = true,
-        undercurl = true,
-        transparent = true,
-        terminalColors = false,
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = "none",
-              },
+  "rebelot/kanagawa.nvim",
+  config = function()
+    require("kanagawa").setup({
+      compile = true,
+      undercurl = true,
+      transparent = true,
+      terminalColors = true,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
             },
           },
         },
-        overrides = function(colors)
-          local theme = colors.theme
-          return {
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
+      },
+      overrides = function(colors)
+        local palette = colors.palette
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none", fg = palette.crystalBlue },
+          FloatTitle = { bg = "none", fg = palette.peachRed },
+          CursorLine = { bg = "none" },
 
-            Pmenu = { fg = "none", bg = "none" },
-            PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
-            PmenuSbar = { fg = theme.ui.bg_p2, bg = "none" },
-            PmenuThumb = { bg = theme.ui.bg_p2 },
-            BlinkCmpMenuBorder = { bg = "none" },
-          }
-        end,
-        theme = "wave",
-      })
-      vim.cmd("colorscheme kanagawa")
-    end,
-  },
+          Pmenu = { bg = "none", fg = "none" },
+          PmenuSel = { bg = palette.waveBlue1, fg = "none" },
+          PmenuThumb = { bg = palette.autumnRed },
+
+          BlinkCmpMenuBorder = { bg = "none", fg = palette.crystalBlue },
+
+          NoiceCmdlineIcon = { fg = palette.crystalBlue },
+
+          SnacksPickerPrompt = { bg = "none", fg = palette.surimiOrange },
+
+          MiniFilesTitle = { bg = "none" },
+          MiniFilesTitleFocused = { bg = "none", fg = palette.surimiOrange },
+        }
+      end,
+      theme = "wave",
+    })
+    vim.cmd("colorscheme kanagawa")
+  end,
 }
