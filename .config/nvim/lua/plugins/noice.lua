fdd2229284b0984108ed41c9a1af6f52233efc03
vedant <vedantd73839@gmail.com>
2025-09-05 19:41:31 +0530

    config modified

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,20 +1,23 @@
 return {
   "folke/noice.nvim",
-  dependencies = { "MunifTanjim/nui.nvim" },
   event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
   opts = {
+    cmdline = {
+      enabled = true,
+      view = "cmdline",
+    },
     presets = {
-      bottom_search = false,
-      command_palette = false,
+      bottom_search = true,
       long_message_to_split = true,
-      inc_rename = true,
+      inc_rename = false,
       lsp_doc_border = true,
     },
     lsp = {
       signature = {
-        auto_open = {
-          enabled = false,
-        },
+        enabled = false,
       },
     },
   },
