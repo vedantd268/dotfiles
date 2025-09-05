7d8e62745a214e2f218c62edba0f4913372316f1
vedant <vedantd73839@gmail.com>
2025-09-05 19:35:16 +0530

    vim.hl set

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 120 })
+    vim.hl.on_yank({ higroup = "IncSearch", timeout = 120 })
   end,
 })
