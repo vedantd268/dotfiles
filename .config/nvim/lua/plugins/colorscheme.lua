9b672563c5e5e33d4c146bf6982ef4c349dbd57d
vedant <vedantd73839@gmail.com>
2025-09-05 19:38:38 +0530

    some highlight removed

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
       undercurl = true,
       transparent = true,
       terminalColors = true,
+      keywordStyle = { italic = false },
       colors = {
         theme = {
           all = {
@@ -19,26 +20,20 @@ return {
         local palette = colors.palette
         return {
           NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none", fg = palette.crystalBlue },
-          FloatTitle = { bg = "none", fg = palette.peachRed },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
           CursorLine = { bg = "none" },
-
           Pmenu = { bg = "none", fg = "none" },
           PmenuSel = { bg = palette.waveBlue1, fg = "none" },
           PmenuThumb = { bg = palette.autumnRed },
-
-          BlinkCmpMenuBorder = { bg = "none", fg = palette.crystalBlue },
-
+          BlinkCmpMenuBorder = { bg = "none" },
+          NoiceCmdline = { bg = "none" },
+          MsgArea = { bg = "none" },
           NoiceCmdlineIcon = { fg = palette.crystalBlue },
-
-          SnacksPickerPrompt = { bg = "none", fg = palette.surimiOrange },
-
-          MiniFilesTitle = { bg = "none" },
-          MiniFilesTitleFocused = { bg = "none", fg = palette.surimiOrange },
+          WinSeparator = { fg = palette.waveBlue1 },
         }
       end,
-      theme = "wave",
     })
-    vim.cmd("colorscheme kanagawa")
+    vim.cmd("colorscheme kanagawa-wave")
   end,
 }
