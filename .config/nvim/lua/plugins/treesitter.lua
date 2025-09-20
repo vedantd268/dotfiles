1ba2ea7158fe9b71e175d89a42bc8c8fb408bf5a
vedant <vedantd73839@gmail.com>
2025-09-20 16:54:35 +0530

    treesitter-textobject added

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  branch = "master",
+  lazy = false,
   build = ":TSUpdate",
   event = "VeryLazy",
   config = function()
@@ -10,26 +15,50 @@ return {
         "vimdoc",
         "query",
         "regex",
-        "sql",
+        "bash",
         "json",
         "jsonc",
         "markdown",
+        "gitignore",
+        "gitattributes",
         "javascript",
         "typescript",
-        "python",
         "tsx",
         "html",
         "css",
         "java",
+        "groovy",
+        "properties",
+        "xml",
         "toml",
         "yaml",
         "tmux",
-        "xml",
+        "hyprlang",
       },
       auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = { query = "@class.inner" },
+            ["as"] = { query = "@local.scope", query_group = "locals" },
+          },
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
       },
     })
   end,
