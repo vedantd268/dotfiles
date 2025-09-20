d9a5c3a6df9736f2503334e49d6e14dc02729596
vedant <vedantd73839@gmail.com>
2025-09-20 16:52:57 +0530

    mason.lua added

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,13 @@
+return {
+  "williamboman/mason.nvim",
+  event = "VeryLazy",
+  opts = {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
+  },
+}
