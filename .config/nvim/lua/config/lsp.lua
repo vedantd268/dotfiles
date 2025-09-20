09b95641eb92e2ba59d00d2855cb73272cd4df6c
vedant <vedantd73839@gmail.com>
2025-09-20 16:51:21 +0530

    lsp.lua added

---
+++ .config/nvim/lua/config/lsp.lua
@@ -0,0 +1,21 @@
+vim.lsp.enable("jdtls")
+vim.lsp.enable("lua_ls")
+
+vim.diagnostic.config({
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.INFO] = "󰋽 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+    },
+  },
+})
