0d9a14f97c8bb27756b80a5f44b2dfd817f6a9e8
vedant <vedantd73839@gmail.com>
2025-09-20 16:59:37 +0530

    modified

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,40 @@
 return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    require("kanagawa").setup({
-      compile = true,
-      undercurl = true,
-      transparent = true,
-      terminalColors = true,
-      keywordStyle = { italic = false },
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
+        undercurl = true,
+        transparent = true,
+        terminalColors = true,
+        keywordStyle = {
+          italic = false,
+        },
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
             },
           },
         },
-      },
-      overrides = function(colors)
-        local palette = colors.palette
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-          CursorLine = { bg = "none" },
-          Pmenu = { bg = "none", fg = "none" },
-          PmenuSel = { bg = palette.waveBlue1, fg = "none" },
-          PmenuThumb = { bg = palette.autumnRed },
-          BlinkCmpMenuBorder = { bg = "none" },
-          NoiceCmdline = { bg = "none" },
-          MsgArea = { bg = "none" },
-          NoiceCmdlineIcon = { fg = palette.crystalBlue },
-          WinSeparator = { fg = palette.waveBlue1 },
-        }
-      end,
-    })
-    vim.cmd("colorscheme kanagawa-wave")
-  end,
+        overrides = function(colors)
+          local palette = colors.palette
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+            CursorLine = { bg = "none" },
+            Pmenu = { bg = "none", fg = "none" },
+            PmenuSel = { bg = palette.waveBlue1, fg = "none" },
+            BlinkCmpMenuBorder = { bg = "none" },
+            -- MsgArea = { bg = "none" },
+            WinSeparator = { fg = palette.waveBlue1 },
+          }
+        end,
+      })
+      vim.cmd("colorscheme kanagawa-wave")
+    end,
+  },
 }
