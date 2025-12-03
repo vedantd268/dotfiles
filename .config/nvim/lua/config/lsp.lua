c32de64044f5690c7fe971c92cb1cafaf7e269cf
vedant <vedantd73839@gmail.com>
2025-12-03 10:46:53 +0530

    html lsp added

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -52,8 +52,6 @@ vim.lsp.config("emmet_ls", {
     "html",
     "htmlangular",
     "htmldjango",
-    "javascriptreact",
-    "typescriptreact",
   },
   root_markers = { ".git" },
 })
@@ -80,6 +78,18 @@ vim.lsp.config("clangd", {
   },
 })
 
+vim.lsp.config("html", {
+  cmd = { "vscode-html-language-server", "--stdio" },
+  filetypes = { "html", "templ" },
+  root_markers = { "package.json", ".git" },
+  settings = {},
+  init_options = {
+    provideFormatter = true,
+    embeddedLanguages = { css = true, javascript = true },
+    configurationSection = { "html", "css", "javascript" },
+  },
+})
+
 vim.lsp.config("css", {
   cmd = { "vscode-css-language-server", "--stdio" },
   filetypes = { "css", "scss", "less" },
@@ -92,61 +102,38 @@ vim.lsp.config("css", {
   },
 })
 
-vim.lsp.config("biome", {
-  {
-    cmd = function(dispatchers, config)
-      local cmd = "biome"
-      local local_cmd = (config or {}).root_dir and config.root_dir .. "/node_modules/.bin/biome"
-      if local_cmd and vim.fn.executable(local_cmd) == 1 then
-        cmd = local_cmd
-      end
-      return vim.lsp.rpc.start({ cmd, "lsp-proxy" }, dispatchers)
-    end,
-    filetypes = {
-      "css",
-      "graphql",
-      "html",
-      "javascript",
-      "javascriptreact",
-      "json",
-      "jsonc",
-      "svelte",
-      "typescript",
-      "typescript.tsx",
-      "typescriptreact",
-      "vue",
-    },
-    workspace_required = true,
-    root_dir = function(bufnr, on_dir)
-      local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
-      root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
-        or vim.list_extend(root_markers, { ".git" })
-      local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
-
-      local filename = vim.api.nvim_buf_get_name(bufnr)
-      local biome_config_files = { "biome.json", "biome.jsonc" }
-
-      biome_config_files = util.insert_package_json(biome_config_files, "biome", filename)
-
-      local is_buffer_using_biome = vim.fs.find(biome_config_files, {
-        path = filename,
-        type = "file",
-        limit = 1,
-        upward = true,
-        stop = vim.fs.dirname(project_root),
-      })[1]
-      if not is_buffer_using_biome then
-        return
-      end
-
-      on_dir(project_root)
-    end,
+vim.lsp.config("vtsls", {
+  cmd = { "vtsls", "--stdio" },
+  init_options = {
+    hostInfo = "neovim",
   },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  root_dir = function(bufnr, on_dir)
+    local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
+    root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
+      or vim.list_extend(root_markers, { ".git" })
+
+    if vim.fs.root(bufnr, { "deno.json", "deno.jsonc", "deno.lock" }) then
+      return
+    end
+
+    local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+    on_dir(project_root)
+  end,
 })
 
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("docker_ls")
+vim.lsp.enable("html")
 vim.lsp.enable("css")
 vim.lsp.enable("emmet_ls")
 vim.lsp.enable("clangd")
-vim.lsp.enable("biome")
+vim.lsp.enable("vtsls")
