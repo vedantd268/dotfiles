55d6141739a6462f645dabfafb62039041cd2a30
vedant <vedantd73839@gmail.com>
2026-05-17 11:43:05 +0530

    new config added

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,78 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  branch = "main",
-  commit = vim.fn.has("nvim-0.12") == 0 and "7caec274fd19c12b55902a5b795100d21531391f" or nil,
-  version = false, -- last release is way too old and doesn't work on Windows
-  build = function()
-    local TS = require("nvim-treesitter")
-    if not TS.get_installed then
-      LazyVim.error("Please restart Neovim and run `:TSUpdate` to use the `nvim-treesitter` **main** branch.")
-      return
-    end
-    -- make sure we're using the latest treesitter util
-    package.loaded["lazyvim.util.treesitter"] = nil
-    LazyVim.treesitter.build(function()
-      TS.update(nil, { summary = true })
-    end)
-  end,
-  event = { "VeryLazy" },
-  cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+  lazy = false,
+  build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "regex",
-        "bash",
-        "json",
-        "jsonc",
-        "markdown",
-        "gitignore",
-        "gitattributes",
-        "javascript",
-        "typescript",
-        "tsx",
-        "html",
-        "css",
-        "java",
-        "groovy",
-        "properties",
-        "xml",
-        "toml",
-        "yaml",
-        "tmux",
-        "hyprlang",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = { query = "@class.inner" },
-            ["as"] = { query = "@local.scope", query_group = "locals" },
-          },
-          selection_modes = {
-            ["@parameter.outer"] = "v", -- charwise
-            ["@function.outer"] = "V", -- linewise
-            ["@class.outer"] = "<c-v>", -- blockwise
-          },
-          include_surrounding_whitespace = false,
-        },
-      },
+    TS = require("nvim-treesitter")
+    TS.setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
     })
+    TS.install({ "rust", "javascript", "zig", "python", "java" })
   end,
 }
